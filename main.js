1b5ee866a73f920dcda15aed1cb08cbc78418cdc
gvibe3 <110994525+Gvibe3@users.noreply.github.com>
2023-09-26 07:08:12 +0300

    function to validate form added

--- main.js
+++ main.js
@@ -8,49 +8,33 @@ function Book(title,author,pages,readStatus){
 
 //add toggle function as prototype for efficiency
 Book.prototype.toggleRead = function(book){
-  //interchange 
-  
-   this.readStatus = !this.readStatus
-  
-   
-}
-
-//function to toggle
+    //interchange value
+    this.readStatus = !this.readStatus
+  }
 
 //put user book as object in the array
 let myLibrary = [];
-
-   
-  
-  
-   
-     
-  
- //function to add user book to the array
+//function to add user book to the array
  
  function addBookToLibrary(array){
- //form value  
+ //form values  
  const userBookTitle = document.getElementById('book-title').value;
  const userBookAuthor = document.getElementById('author').value;
  const userBookPages = document.getElementById('pages').value;
  const userReadStatus = document.getElementById('read-it').checked;
-  
-   
-  
 
-    let userBook = new Book(userBookTitle,userBookAuthor,userBookPages,userReadStatus);
-    
+ 
+  
+   let userBook = new Book(userBookTitle,userBookAuthor,userBookPages,userReadStatus);
     array.push(userBook)
  }
- //function to change status
  
-
  //function that accept array of books object and  create card for each book in the array
  function bookCards(myarray){
 
     myarray.forEach(book => {
         
-   const books = document.querySelector('.books')
+    const books = document.querySelector('.books')
     const card = document.createElement('div')
     const bookTitle = document.createElement('h3');
     const bookAuthor = document.createElement('h4');
@@ -79,16 +63,12 @@ let myLibrary = [];
     books.appendChild(card)
 
     //text content for items
-    bookTitle.textContent = book.title
-    bookAuthor.textContent = "by" + book.author
-    bookPage.textContent = "pages: " + book.pages
-    //checkbok
-     
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = "by " + book.author;
+    bookPage.textContent = "pages: " + book.pages;
     bookStatus.textContent = book.readStatus ? "Completed" : 'Not read it yet.'
-    
-    
-    deleteBtn.textContent = "Remove"
-    read.textContent = "change Book status"
+    deleteBtn.textContent = "Remove";
+    read.textContent = "change status";
 
      //use event delegation on the card a to remove the userbook from the array using remove buttton  
       card.addEventListener('click',(e)=>{
@@ -109,12 +89,8 @@ let myLibrary = [];
               
             }
           });
-    
     }); 
-
- }
-
- 
+}
  const addBooks = document.querySelector('.add-book');
  const modal = document.querySelector('.book-info');
  const submit = modal.querySelector('.confirm-btn');
@@ -125,6 +101,7 @@ function displayBook(myarray){
      // clear the books to avoid new one writing thr old
       books.innerHTML = "";
       bookCards(myarray);
+     
   }
 
 //button to  show modal
@@ -136,7 +113,51 @@ addBooks.addEventListener('click',()=>{
 
 submit.addEventListener('click',function(event){
     event.preventDefault();
-    modal.close();
-    addBookToLibrary(myLibrary);
-    displayBook(myLibrary)
+    
+    let userNotRead = document.getElementById('not-read').checked;
+    let userBookTitle = document.getElementById('book-title').value;
+    let userBookAuthor = document.getElementById('author').value;
+    let userBookPages = document.getElementById('pages').value;
+    let userReadStatus = document.getElementById('read-it').checked;
+    const msg = document.querySelector('.validate-msg')
+    const msgFill = document.querySelector('.msg-invalid');
+    //functiion to vallidate
+    function validateForm(){
+        if(userReadStatus == false && userNotRead == false ){
+            msg.style.display = 'block' 
+            msg.textContent="Please mark one of the checkbox."
+            return false
+         }
+         else if(userReadStatus == true && userNotRead == true){
+          msg.style.display = 'block' 
+          msg.textContent = "Please,mark only one of the checkbox."
+          console.log((userReadStatus == true && userNotRead == true))
+          return false
+         }
+        else if(userBookTitle == "" || userBookAuthor == '' || userBookPages == '' ){
+            msgFill.style.display = 'block'
+            msgFill.textContent = "plaease fill all the input."
+            console.log((userBookTitle == null || userBookAuthor == null || userBookPages == null ))
+            return false
+         }
+    }
+    
+    
+   if(validateForm() == false){
+    return
+   }
+   else{
+      modal.close();
+      addBookToLibrary(myLibrary);
+      displayBook(myLibrary);
+
+      //clear the previous input
+    const forms = document.querySelectorAll('input');
+    forms.forEach(input=>{
+      input.value = ""
+      input.checked = false
+    });
+   }
+   
+    
 });
